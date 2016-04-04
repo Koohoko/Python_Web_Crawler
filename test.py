574cdbe6eb76bd3f68d0af25da28b302018bf68c
Koohoko <koohoko@gmail.com>
2016-04-04 22:39:03 +0800

    first commit

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+
 print 'hello world'
+print 45678 + 0x12fd2
+print r'''"To be, or not to be": that is the question.
+Whether it's nobler in the mind to suffer."'''
+
+x = 0
+L = []
+for i in range(10):
+    x = x + i
+    L.append(x)
